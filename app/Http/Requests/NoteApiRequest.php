4c02f4ffcb07bf8a8e8b30320bd681b63d3ed96e
KatherineEvans <Evans.Katherine.E@gmail.com>
2021-09-26 11:58:46 -0500

    add custom validation messages

--- app/Http/Requests/NoteApiRequest.php
+++ app/Http/Requests/NoteApiRequest.php
@@ -3,6 +3,8 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class NoteApiRequest extends FormRequest
 {
@@ -28,4 +30,26 @@ class NoteApiRequest extends FormRequest
             'note' => 'nullable|max:1000',
         ];
     }
+
+    /**
+     * Get the validation custom messages
+     *
+     * @return array
+     */
+    public function messages() {
+        return [
+        'title.required'       => 'Note title is required.',
+        'title.max'      => 'Note titles cannot be more than 50 characters in length.',
+        'note.max'      => 'Notes cannot be more than 1000 characters in length.',
+        ];
+    }
+    
+    /**
+     * Override failedValidation to return json response without redirect. 
+     */
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json($validator->errors(), 422));
+    }
+
 }
