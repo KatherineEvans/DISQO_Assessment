1fcf7e99c9e078b1ea37b5927e8dd2739de5e2bf
KatherineEvans <Evans.Katherine.E@gmail.com>
2021-09-25 19:59:55 -0500

    crate note migration file and model

---
+++ app/Models/Note.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Note extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'title',
+        'note',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+        'deleted_at' => 'datetime',
+    ];
+
+    /******************************************************
+     * Relationships
+     ******************************************************/
+
+    /**
+     * Get the user for a note.
+     */
+    public function notes()
+    {
+        return $this->belongsTo('App\Models\User');
+    }
+
+    /******************************************************
+     * Scope
+     ******************************************************/
+
+    /**
+     * Scope notes to user.
+     */
+    public function scopeThisUser($query)
+    {
+        // If user, show notes for said user only.
+        if(auth()->user()->id){
+            return $query->where('user_id', auth()->user()->id);
+        }
+
+        abort(403);
+    }
+
+}
---
+++ database/migrations/2021_09_25_230556_create_notes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNotesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notes', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id');
+            $table->string('title');
+            $table->text('note');
+            $table->timestamps();
+            $table->date('deleted_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notes');
+    }
+}
