c2f8f14361583191d3afae0e5a59232626a19d3e
KatherineEvans <Evans.Katherine.E@gmail.com>
2021-12-20 18:20:37 -0600

    updates to routes file

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->group(function() {
     Route::apiResource('notes', NotesController::class);
+    Route::apiResource('comments', CommentController::class);
 });
